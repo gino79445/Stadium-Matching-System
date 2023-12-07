d1764c6aa2a8cfc633d223c44f4aaaa1d6b085e7
Ubuntu <ubuntu@ip-172-31-42-18.ap-northeast-1.compute.internal>
2023-12-07 09:45:08 +0000

    modify nginx

--- Route/app.js
+++ Route/app.js
@@ -29,7 +29,7 @@ app.get('/health', (req, res) => {
     return res.status(200).send('OK');
 });
 
-
+//app.use('/.well-known/pki-validation', express.static('/.well-known/pki-validation/'));
 app.use('/static', express.static('/home/ubuntu/Stadium-Matching-System/app'));
 app.use('/default_images', express.static(process.env.DEFAULT_PROFILE_PATH));
 app.use('/api/user', userRouter);
