791ee7bf712a1392fa3378634fc4f1ab87c614ec
yjchen0228 <yjchen0228@gmail.com>
2023-12-20 16:33:53 +0800

    renew SAS

--- utils/azureBlobService.js
+++ utils/azureBlobService.js
@@ -1,7 +1,7 @@
 // azureBlobService.js
 const { BlobServiceClient } = require('@azure/storage-blob');
 
-const sasUrl = 'https://stadiumafab3efe.blob.core.windows.net/images?sp=racwdli&st=2023-12-19T17:08:25Z&se=2023-12-20T01:08:25Z&sv=2022-11-02&sr=c&sig=RG2Rx5IzMKogG8DaCLuS1WahHk0e7GjYBofzOmGzWzM%3D';
+const sasUrl = 'https://stadiumafab3efe.blob.core.windows.net/images?sp=racwdli&st=2023-12-20T08:27:30Z&se=2024-11-30T16:27:30Z&spr=https&sv=2022-11-02&sr=c&sig=Y9rVIdPsomRdmMj1ukC1SkxlxXNgjB33QN93I5IWhZo%3D';
 const blobServiceClient = new BlobServiceClient(sasUrl);
 const containerName = 'images'; // Your container name
 
@@ -12,5 +12,4 @@ async function uploadBlob(fileBuffer, blobName) {
     await blockBlobClient.upload(fileBuffer, fileBuffer.length);
     return blockBlobClient.url;
 }
-
 module.exports = { uploadBlob };
